e541b82aad41e51239203100e08889aa4c9d77f2
avilchisrdz <49501288+avilchisrdz@users.noreply.github.com>
2024-07-30 02:14:01 -0600

    Primer Avance De Notificación
    Se agrega notificación visual, con tipos de notificación y control del botón de envío. Agregué carpeta de ayuda public para usos posteriores. Form manual para analizar el request de la Api de terceros y la notificación.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,113 @@ const ctnList = document.querySelector('.lists');
 const ListH = document.querySelector('.list-Ham');
 const alert = document.querySelector('.enviar');
 
-logoHam.addEventListener('click', () =>{
-    logoHam.classList.toggle('active');
-    ctnList.classList.toggle('abrir');
-    ListH.classList.toggle('block');
+logoHam.addEventListener('click', () => {
+	logoHam.classList.toggle('active');
+	ctnList.classList.toggle('abrir');
+	ListH.classList.toggle('block');
 })
 
+document.getElementById('form-contact').addEventListener('submit', function (event) {
+	event.preventDefault();
+	if (true) {
+		addNotify('Mensaje enviado', `ok`);//ok
+    disableBtn("buttonModal", 5, true);
+    this.submit();
+	} else {
+		addNotify('Mensaje enviado', `danger`);//error
+		addNotify('Mensaje enviado', `caution`);//caution
+	}
+});
 
+function addNotify(messageText = ``, type = `caution`, seconds = 5) {
+	const container = document.getElementById('ctn-notifs');
+	const notify = createNotificationItem(messageText, type);
+	container.appendChild(notify);
+
+	setTimeout(() => {
+		deleteNotificationItem(notify);
+	}, seconds * 1000);
+}
+function createNotificationItem(messageText, type) {
+	const notify = document.createElement('div');
+	notify.className = 'item-notif';
+  const bgNeed = {
+    'ok' : '#d4edda',
+    'caution' : '#ffc107',
+    'danger' : '#dc3545',
+  };
+	const typeNeed = {
+		'ok': 'bi bi-check',
+		'caution': 'bi bi-exclamation-circle',
+		'danger': 'bi bi-exclamation-triangle',
+	};
+	const bg = bgNeed[type] || '#ffc107';
+  console.log('bg: ', bg);
+	const iconClass = typeNeed[type] || 'bi bi-exclamation-circle';
+	const iconLeft = createIcon(iconClass, 'left');
+	const message = createMessage(messageText);
+	// const iconRight = createIcon('right');
+	const deleteButton = createDeleteButton();
+
+  notify.style.backgroundColor = bg;
+
+	notify.appendChild(iconLeft);
+	notify.appendChild(message);
+	notify.appendChild(deleteButton);
+
+	return notify;
+}
+
+function createIcon(iconClass, position) {
+	const icon = document.createElement('div');
+	icon.className = `icon ${position}`;
+
+	const iconElement = document.createElement('i');
+	iconElement.className = `bi ${iconClass}`;
+
+	icon.appendChild(iconElement);
+	return icon;
+}
+function createMessage(text) {
+	const message = document.createElement('div');
+	message.className = 'message';
+	message.textContent = text;
+	return message;
+}
+function createDeleteButton() {
+	const button = document.createElement('button');
+	button.className = 'btn-delete icon right';
+  button.type = 'button';
+	button.innerHTML = '&times;';
+	button.onclick = function () {
+		this.parentElement.remove();
+	};
+	return button;
+}
+
+function deleteNotificationItem(item) {
+	item.remove();
+}
+
+function disableBtn(id, time, forceDisable) {
+  const button = document.getElementById(id);
+
+  if (forceDisable === null) {
+      return;
+  }
+
+  const setButtonState = (disable) => {
+      button.disabled = disable;
+      button.style.backgroundColor = disable ? `grey` : ``;
+      button.style.border = disable ? `0px solid` : ``;
+  };
+
+  if (time === 0 || time === null) {
+      setButtonState(forceDisable);
+  } else {
+      setButtonState(forceDisable);
+      setTimeout(() => {
+          setButtonState(!forceDisable);
+      }, time * 1000);
+  }
+}
